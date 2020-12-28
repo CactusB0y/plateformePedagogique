073276150ec9e238f315b6b648221197e4a4deaa
CactusB0y <68423704+CactusB0y@users.noreply.github.com>
2020-12-28 11:39:40 +0100

    base de donnée

--- database/migrations/2014_10_12_102748_create_fichiers_table.php
+++ database/migrations/2014_10_12_102748_create_fichiers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateFichiersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('fichiers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('prenom');
-            $table->integer('age');
+            $table->string('nom');
             $table->string('src');
-            $table->string('role')->default('membre');
             $table->unsignedBigInteger('matiere_id');
             $table->foreign('matiere_id')->references("id")->on('matieres');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -37,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('fichiers');
     }
 }
